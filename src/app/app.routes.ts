e4cfe3ed2d052fac065b9c4ede03fe8658d48a63
007DSJ <shinobigamerr565@gmail.com>
2024-04-11 23:45:33 +0530

    Added RouterLink to buttons of app.component.html

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,12 @@
-import { Routes } from '@angular/router';
+import { Routes, RouterLink } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { ContactUsComponent } from './contact-us/contact-us.component';
 import { ViewAppointmentComponent } from './view-appointment/view-appointment.component';
 import { PlaceFitnessAppointmnetComponent } from './place-fitness-appointmnet/place-fitness-appointmnet.component';
 
 export const routes: Routes = [
-    {path:'/',component:LandingPageComponent},
-    {path:'/contact-us',component:ContactUsComponent},
+    {path:'',component:LandingPageComponent},
+    {path:'contact-us',component:ContactUsComponent},
     {path:'view-appointment',component:ViewAppointmentComponent},
     {path:'place-appointment',component:PlaceFitnessAppointmnetComponent}
 ];
