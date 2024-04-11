66290a47d80bee948a33cd799f857859fa5a1c4d
007DSJ <shinobigamerr565@gmail.com>
2024-04-12 00:27:23 +0530

    added contact-us form logic to .ts file

--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -1,12 +1,35 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-us',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './contact-us.component.html',
   styleUrl: './contact-us.component.css'
 })
 export class ContactUsComponent {
+  queryForm: FormGroup;
 
+  constructor(private formBuilder: FormBuilder, private http: HttpClient) {
+    this.queryForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      phone: ['', [Validators.required, Validators.pattern("^[0-9]*$")]],
+      message: ['', [Validators.required, Validators.maxLength(200)]]
+    });
+  }
+
+  submitQuery() {
+    if (this.queryForm.valid) {
+      // Form is valid, show alert
+      window.alert('Query submitted successfully!');
+      // Optionally, you can reset the form after submission
+      this.queryForm.reset();
+    } else {
+      // Form is invalid, display error messages
+      window.alert('Please fill all required fields correctly.');
+    }
+  }
 }
