edf2b8e3df425b174119447ed72659309f10436b
007DSJ <shinobigamerr565@gmail.com>
2024-04-12 00:30:11 +0530

    view-appointment UI and logic updated

--- src/app/view-appointment/view-appointment.component.ts
+++ src/app/view-appointment/view-appointment.component.ts
@@ -1,4 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+
+interface Appointment {
+  id: number;
+  name: string;
+  address: string;
+  city: string;
+  package: string;
+  trainerPreference: string;
+  phone: string;
+}
 
 @Component({
   selector: 'app-view-appointment',
@@ -8,5 +19,18 @@ import { Component } from '@angular/core';
   styleUrl: './view-appointment.component.css'
 })
 export class ViewAppointmentComponent {
+  appointments: Appointment[] = [];
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.fetchAppointments();
+  }
+
+  fetchAppointments() {
+    this.http.get<Appointment[]>('http://your-api-endpoint/appointments') // Replace with your actual API endpoint
+      .subscribe(data => {
+        this.appointments = data;
+      });
+  }
 }
