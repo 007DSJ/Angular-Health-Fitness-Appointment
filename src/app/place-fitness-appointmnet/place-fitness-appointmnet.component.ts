73abb2aaec4722b8aa224f9ceb2693ed25454e46
007DSJ <shinobigamerr565@gmail.com>
2024-04-12 17:01:34 +0530

    Completed place-appointment post request to db.json

--- src/app/place-fitness-appointmnet/place-fitness-appointmnet.component.ts
+++ src/app/place-fitness-appointmnet/place-fitness-appointmnet.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, NgForm, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ServicesComponent } from '../services/services.component';
 import { HttpClient } from '@angular/common/http';
 
 export class Fitness {
   constructor(
+    public id: number,
     public inr: number,
     public paisa: number,
     public streetaddress: string,
@@ -15,37 +16,38 @@ export class Fitness {
     public pincode: number,
     public phonenumber: number,
     public email: string,
-    public firstname:string,
+    public firstname: string,
     public lastname: string,
-    public age:number,
+    public age: number,
     public trainerpreference: string,
     public physiotherapist: string,
     public packages: string,
+
   ) { }
 }
 
 @Component({
   selector: 'app-place-fitness-appointmnet',
   standalone: true,
-  imports: [ReactiveFormsModule,CommonModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './place-fitness-appointmnet.component.html',
   styleUrl: './place-fitness-appointmnet.component.css'
 })
 
-export class PlaceFitnessAppointmnetComponent implements OnInit{
+export class PlaceFitnessAppointmnetComponent implements OnInit {
 
-  fitnessForm:any
-  userService:any
-  amt!: number;
-  appointments : Fitness[]=[];
+  fitnessForm: any
+  userService: any
+  amt: any;
+  appointments: Fitness[] = [];
   url = 'http://localhost:3000/appointments';
 
-  constructor(private formBuilder : FormBuilder, private http : HttpClient){}
+  constructor(private formBuilder: FormBuilder, private http: HttpClient) { }
 
   ngOnInit() {
     this.fitnessForm = this.formBuilder.group({
-      inr: ['', Validators.required],
-      paisa: ['', Validators.required],
+      inr: ['',],
+      paisa: ['',],
       streetaddress: ['', Validators.required],
       city: ['', Validators.required],
       state: ['', Validators.required],
@@ -59,29 +61,24 @@ export class PlaceFitnessAppointmnetComponent implements OnInit{
       trainerpreference: [''],
       physiotherapist: ['false'],
       packages: [''],
-      weeks: ['2',Validators.required], //extra
-      amount: [{ value: '', disabled: true }] // Disable amount initially-extra
+      weeks: ['2', Validators.required], //extra
+      amount: [''] // Disable amount initially-extra
     });
 
   }
 
-  ngOnChange()
-  {
+  ngOnChange() {
     this.fitnessForm.get('packages').valueChanges.subscribe(() => this.updateAmount()); //extra
     this.fitnessForm.get('weeks').valueChanges.subscribe(() => this.updateAmount()); //extra
     this.fitnessForm.get('physiotherapist').valueChanges.subscribe(() => this.updateAmount()); //extra
   }
 
-  updateAmount()
-  {
+  updateAmount() {
     // extra
-    const packageValue = this.fitnessForm.get('package').value;
+    const packageValue = this.fitnessForm.get('packages').value;
     const weeksValue = this.fitnessForm.get('weeks').value;
     const physio = this.fitnessForm.get('physiotherapist').value
 
-    // Calculate amount based on selected package and weeks
-    // Add logic to calculate the amount based on the selected package and weeks
-    // For demonstration, let's assume a simple calculation
     if (packageValue === 'Package A') {
       this.amt += weeksValue * 100;
     } else if (packageValue === 'Package B') {
@@ -90,43 +87,106 @@ export class PlaceFitnessAppointmnetComponent implements OnInit{
       this.amt += weeksValue * 200;
     }
 
-    if(physio===true)
-      {
-        this.amt+=200;
-      }
+    if (physio === true) {
+      this.amt += 200;
+    }
 
     // Update the amount field in the form
     this.fitnessForm.get('amount').patchValue(this.amt);
+
+
+    // Calculate amount based on selected package and weeks
+    // Add logic to calculate the amount based on the selected package and weeks
+    // For demonstration, let's assume a simple calculation
+
   }
 
   onSubmit() {
+
+    var amt=0;
+
     this.fitnessForm.value;
     console.log(
       "LOG: LoginComponent -> onSubmit -> this.fitnessForm.value",
       this.fitnessForm.value
     );
-    if (this.fitnessForm.invalid) {
-      return;
+    console.log(this.amt);
+
+    if (this.fitnessForm.valid) {
+      const fitnessData = this.fitnessForm.value;
+      
+    if (fitnessData.packages === '500') {
+      amt += fitnessData.weeks * 500;
+    } else if (fitnessData.packages === '400') {
+      amt += fitnessData.weeks * 400;
+    } else if (fitnessData.packages === '300') {
+      amt += fitnessData.weeks * 300;
     }
-    const fitnessData = this.fitnessForm.value;
-    const fitness = new Fitness(
-      fitnessData.inr,
-      fitnessData.paisa,
-      fitnessData.streetaddress,
-      fitnessData.city,
-      fitnessData.state,
-      fitnessData.country,
-      fitnessData.pincode,
-      fitnessData.phonenumber,
-      fitnessData.email,
-      fitnessData.firstname,
-      fitnessData.lastname,
-      fitnessData.age,
-      fitnessData.trainerpreference,
-      fitnessData.physiotherapist,
-      fitnessData.packages
-    );
-    
+
+    if (fitnessData.physiotherapist === true) {
+      amt += 200;
+    }
+
+    // Update the amount field in the form
+    this.fitnessForm.get('amount').patchValue(amt);
+      fitnessData.inr = amt;
+      const fitness = new Fitness(
+        fitnessData.id,
+        fitnessData.inr,
+        fitnessData.paisa,
+        fitnessData.streetaddress,
+        fitnessData.city,
+        fitnessData.state,
+        fitnessData.country,
+        fitnessData.pincode,
+        fitnessData.phonenumber,
+        fitnessData.email,
+        fitnessData.firstname,
+        fitnessData.lastname,
+        fitnessData.age,
+        fitnessData.trainerpreference,
+        fitnessData.physiotherapist,
+        fitnessData.packages,
+
+      );
+
+      const newAppointment = {
+        name: fitnessData.firstname,
+        age: fitnessData.age,
+        phone: fitnessData.phonenumber,
+        email: fitnessData.email,
+        streetaddress: fitnessData.streetaddress,
+        city: fitnessData.city,
+        state: fitnessData.state,
+        country: fitnessData.country,
+        pincode: fitnessData.pincode,
+        trainerpreference: fitnessData.trainerpreference,
+        physiotherapist: fitnessData.physiotherapist,
+        packages: fitnessData.packages,
+        inr: fitnessData.inr,
+        paisa: 0,
+      };
+
+
+      this.http.post(this.url, newAppointment) // Replace with your actual API endpoint
+        .subscribe((data) => {
+          console.log('POST Request is successful ', data);
+        });
+
+      this.fitnessForm.reset();
+    }
+
+    else {
+      window.alert('Invalid Data in Form . fill the form properly');
+      console.error();
+
+    }
+
+    // if (this.fitnessForm.invalid) {
+    //   return;
+    // }
+
+
     // this.userService.postfitnessdata(fitness)
     // .subscribe(
     //   (response: any) => {
@@ -138,13 +198,7 @@ export class PlaceFitnessAppointmnetComponent implements OnInit{
     //   }
     // );
 
-    // this.http.post(this.url,fitness) // Replace with your actual API endpoint
-    // .subscribe((data) => {
-    //   console.log('POST Request is successful ', data);
-    // },
-    // error => {
-    //   console.log('Error', error);
-    // });
+
 
   }
 }
