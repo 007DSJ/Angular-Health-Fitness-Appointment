293a5e68e877ab6e0e5e603d9131712672f5bc01
007DSJ <shinobigamerr565@gmail.com>
2024-04-12 11:03:01 +0530

    to stop redirect use this

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { RouterOutlet,RouterLink, Router } from '@angular/router';
@@ -6,13 +7,14 @@ import { RouterOutlet,RouterLink, Router } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,CommonModule,RouterLink],
+  imports: [RouterOutlet,CommonModule,RouterLink,HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
 
   constructor(private router : Router){}
+  // Add HttpClientModule here otherwise the app will redirect itself
 
   navpage() {
     this.router.navigate(['/place-appointment']);
